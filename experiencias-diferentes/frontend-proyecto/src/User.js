c5f8e7347cc1cc110b6830b3896171e91ea6c6b8
alberto-cora <albertocora@hotmail.com>
2021-06-21 20:09:42 +0200

    correcciones codigo sobrante users.js

--- experiencias-diferentes/frontend-proyecto/src/User.js
+++ experiencias-diferentes/frontend-proyecto/src/User.js
@@ -1,43 +1,45 @@
-import "./User.css";
-import { useParams } from "react-router-dom";
-import useFetch from "./useFetch";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import './User.css';
+import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function User() {
-  const me = useSelector((s) => s.user);
-  const id = useParams()?.id || me?.id;
-  const user = useFetch(`http://localhost:3080/api/users/${id}`);
+    const me = useSelector((s) => s.user);
+    const id = useParams()?.id || me?.id;
+    const user = useFetch(`http://localhost:3080/api/users/${id}`);
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="user">
-      <h3 className="info">
-        - ID USUARIO : <i> {user.id}</i>
-      </h3>
-      <h3 className="info">
-        - EMAIL USUARIO : <i> {user.email}</i>
-      </h3>
-      <h3 className="info">
-        - NOMBRE USUARIO : <i> {user.name}</i>
-      </h3>
-      <h3 className="info">
-        activity/create
-        <img src={user.image} alt="" />
-      </h3>
-      <button>
-        <NavLink to={`/user/${id}/update`} activeClassName="active" exact>
-          Editar datos usuario
-        </NavLink>
-      </button>
-    </div>
-  );
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="user">
+            <h3 className="info">
+                - ID USUARIO : <i> {user.id}</i>
+            </h3>
+            <h3 className="info">
+                - EMAIL USUARIO : <i> {user.email}</i>
+            </h3>
+            <h3 className="info">
+                - NOMBRE USUARIO : <i> {user.name}</i>
+            </h3>
+            <h3 className="info">
+                activity/create
+                <img src={user.image} alt="" />
+            </h3>
+            <button>
+                <NavLink
+                    to={`/user/${id}/update`}
+                    activeClassName="active"
+                    exact
+                >
+                    Editar datos usuario
+                </NavLink>
+            </button>
+        </div>
+    );
 }
 
 export default User;
 
 //VER BIOGRAFÍA? FALTA EN BACKEND
-// const url = path.join(__dirname, `../static/users/${id_user}/${file.filename}`);
-// const updateUser = await usersRepository.updateAvatar(url, id_user);
