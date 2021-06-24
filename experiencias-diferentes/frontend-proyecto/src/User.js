a54d2ad4d28c5f9948e3bd1ef041b92fc105d476
alberto-cora <albertocora@hotmail.com>
2021-06-24 16:14:17 +0200

    añadimos mejora info user

--- experiencias-diferentes/frontend-proyecto/src/User.js
+++ experiencias-diferentes/frontend-proyecto/src/User.js
@@ -3,17 +3,13 @@ import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 function User() {
     const me = useSelector((s) => s.user);
     const id = useParams()?.id || me?.id;
     const user = useFetch(`http://localhost:3080/api/users/${id}`);
-    const activities = useFetch(
-        `http://localhost:3080/api/activities/users/${id}`
-    );
 
-    if (!user || !activities) {
+    if (!user) {
         return <div>Loading...</div>;
     }
     return (
@@ -40,52 +36,6 @@ function User() {
                         Editar datos usuario
                     </NavLink>
                 </button>
-            </div>
-
-            <div className="user-activities">
-                <h2>Actividades</h2>
-                <div>
-                    {activities &&
-                        activities?.map((activity) => (
-                            <li>
-                                <div className="search-results">
-                                    <Link to={`/activity/${activity.id}`}>
-                                        Ir a...{activity.title}
-                                    </Link>
-                                    <br />
-                                    <img
-                                        src={activity.image}
-                                        alt="foto-actividad"
-                                    />
-                                    <p>
-                                        <strong>Descripción:</strong>
-                                        {activity.description}
-                                    </p>
-                                    <p>
-                                        <strong>Precio:</strong>
-                                        {activity.price}€
-                                    </p>
-                                    <p>
-                                        <strong>Lugar:</strong>
-                                        {activity.location}
-                                    </p>
-                                    <p>
-                                        <strong>Fecha inicio:</strong>
-                                        {activity.startDate}
-                                    </p>
-                                    <p>
-                                        Search.css
-                                        <strong>Plazas totales:</strong>
-                                        {activity.totalPlaces}
-                                    </p>
-                                </div>
-                            </li>
-                        ))}
-                </div>
-                {!activities && <i>Loading...</i>}
-                {activities && activities.length === 0 && (
-                    <i>No hay actividades!</i>
-                )}
             </div>
         </div>
     );
