62242f08555de2c4877455a10df8ae0a08888c43
JVare <javier.varela.prieto@gmail.com>
2021-06-25 18:08:46 +0200

    fechas modificadas

--- experiencias-diferentes/frontend-proyecto/src/Profile.js
+++ experiencias-diferentes/frontend-proyecto/src/Profile.js
@@ -9,102 +9,93 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 function UserBookings() {
-    const me = useSelector((s) => s.user);
-    const id = useParams()?.id || me?.id;
-    const activities = useFetch(
-        `http://localhost:3080/api/activities/users/${id}`
-    );
+  const me = useSelector((s) => s.user);
+  const id = useParams()?.id || me?.id;
+  const activities = useFetch(
+    `http://localhost:3080/api/activities/users/${id}`
+  );
 
-    if (!activities) {
-        return <div>Loading...</div>;
-    }
-    return (
+  if (!activities) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <div className='user-activities'>
         <div>
-            <div className="user-activities">
-                <div>
-                    {activities &&
-                        activities?.map((activity) => (
-                            <li>
-                                <div className="search-results">
-                                    <Link to={`/activity/${activity.id}`}>
-                                        Ir a...{activity.title}
-                                    </Link>
-                                    <br />
-                                    <img
-                                        src={activity.image}
-                                        alt="foto-actividad"
-                                    />
-                                    <p>
-                                        <strong>Descripción:</strong>
-                                        {activity.description}
-                                    </p>
-                                    <p>
-                                        <strong>Precio:</strong>
-                                        {activity.price}€
-                                    </p>
-                                    <p>
-                                        <strong>Lugar:</strong>
-                                        {activity.location}
-                                    </p>
-                                    <p>
-                                        <strong>Fecha inicio:</strong>
-                                        {activity.startDate}
-                                    </p>
-                                    <p>
-                                        <strong>Fecha fin:</strong>
-                                        {activity.endDate}
-                                    </p>
-                                </div>
-                            </li>
-                        ))}
+          {activities &&
+            activities?.map((activity) => (
+              <li>
+                <div className='search-results'>
+                  <Link to={`/activity/${activity.id}`}>
+                    Ir a...{activity.title}
+                  </Link>
+                  <br />
+                  <img src={activity.image} alt='foto-actividad' />
+                  <p>
+                    <strong>Descripción:</strong>
+                    {activity.description}
+                  </p>
+                  <p>
+                    <strong>Precio:</strong>
+                    {activity.price}€
+                  </p>
+                  <p>
+                    <strong>Lugar:</strong>
+                    {activity.location}
+                  </p>
+                  <p>
+                    <strong>Fecha inicio:</strong>
+                    {new Date(activity.startDate).toLocaleDateString()}
+                  </p>
+                  <p>
+                    <strong>Fecha fin:</strong>
+                    {new Date(activity.endDate).toLocaleDateString()}
+                  </p>
                 </div>
-                {!activities && <i>Loading...</i>}
-                {activities && activities.length === 0 && (
-                    <i>No hay actividades!</i>
-                )}
-            </div>
+              </li>
+            ))}
         </div>
-    );
+        {!activities && <i>Loading...</i>}
+        {activities && activities.length === 0 && <i>No hay actividades!</i>}
+      </div>
+    </div>
+  );
 }
 
 function Profile() {
-    // const isLoggedIn = useSelector((s) => !!s.user);
+  // const isLoggedIn = useSelector((s) => !!s.user);
 
-    // if (!isLoggedIn) return <Redirect to="/login" />;
+  // if (!isLoggedIn) return <Redirect to="/login" />;
 
-    return (
-        <div className="profile">
-            <h1>Perfil</h1>
-            <Helmet>
-                <title>Experiencias diferentes - Mi Perfil</title>
-            </Helmet>
-            <div className="box">
-                <div className="tabs">
-                    <NavLink to="/profile" exact activeClassName="active">
-                        Información
-                    </NavLink>
+  return (
+    <div className='profile'>
+      <h1>Perfil</h1>
+      <Helmet>
+        <title>Experiencias diferentes - Mi Perfil</title>
+      </Helmet>
+      <div className='box'>
+        <div className='tabs'>
+          <NavLink to='/profile' exact activeClassName='active'>
+            Información
+          </NavLink>
 
-                    <NavLink
-                        to="/profile/bookings"
-                        exact
-                        activeClassName="active"
-                    >
-                        Historial
-                    </NavLink>
-                </div>
-                <div className="content">
-                    <Switch>
-                        <Route path="/profile" exact>
-                            <User />
-                        </Route>
-                        <Route path="/profile/bookings" exact>
-                            <UserBookings />
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
+          <NavLink to='/profile/bookings' exact activeClassName='active'>
+            Historial
+          </NavLink>
         </div>
-    );
+        <div className='content'>
+          <Switch>
+            <Route path='/profile' exact>
+              <User />
+            </Route>
+            <Route path='/profile/bookings' exact>
+              <UserBookings />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
