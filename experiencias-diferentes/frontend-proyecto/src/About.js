fef72053da914458596fea929052bb8a15dd04dd
alberto-cora <albertocora@hotmail.com>
2021-06-27 20:24:53 +0200

    añadido texto about y where

--- experiencias-diferentes/frontend-proyecto/src/About.js
+++ experiencias-diferentes/frontend-proyecto/src/About.js
@@ -4,11 +4,17 @@ import './About.css';
 function About() {
     return (
         <div className="about">
-            <h1>About us</h1>
+            <h1>
+                About us: We are a better company when talking about the unique
+                experiencies.
+            </h1>
             <Helmet>
                 <title>Experiencias diferentes - About us</title>
             </Helmet>
-            We are a better company when talking about the unique experiencies
+            La idea para desarrollar una web intuitiva y de sencilla navegación
+            que ofrezca experiencias diferentes y variadas a un precio razonable
+            y con amplio abanico de fechas nace entre dos amigos quienes
+            afrontan este reto con mucha ilusión y esfuerzo.
             <span className="trick"></span>
         </div>
     );
--- experiencias-diferentes/frontend-proyecto/src/Where.js
+++ experiencias-diferentes/frontend-proyecto/src/Where.js
@@ -2,16 +2,19 @@ import Helmet from 'react-helmet';
 import './Where.css';
 
 function Where() {
-  return (
-    <div className='where'>
-      <h1>Dónde estamos</h1>
-      <Helmet>
-        <title>Experiencias diferentes - Dónde estamos...</title>
-      </Helmet>
-      Nuestras oficinas centrales se encuentran situadas en A Coruña.
-      <span className='foto-where'></span>
-    </div>
-  );
+    return (
+        <div className="where">
+            <h1>Dónde estamos</h1>
+            <Helmet>
+                <title>Experiencias diferentes - Dónde estamos...</title>
+            </Helmet>
+            Nuestras oficinas centrales se encuentran situadas en una zona rural
+            de la provincia de A Coruña. Y trabajamos con otras empresas locales
+            las cuales nos alquilan el material necesario para cada disciplina.¡
+            según donde se vaya a desarrollar cada actividad.
+            <span className="foto-where"></span>
+        </div>
+    );
 }
 
 export default Where;
