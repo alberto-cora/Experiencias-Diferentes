43fcb8e6a6c1d16643eac6bfdfa7d0f9b65e5c0f
JVare <javier.varela.prieto@gmail.com>
2021-06-27 19:32:01 +0200

    Titulo pagina

--- experiencias-diferentes/frontend-proyecto/src/Home.js
+++ experiencias-diferentes/frontend-proyecto/src/Home.js
@@ -2,18 +2,18 @@ import Helmet from 'react-helmet';
 import Search from './Search';
 import './Home.css';
 function Home() {
-    return (
-        <div className="home" id="home">
-            <Helmet>
-                <title>Experiencias diferentes - Home</title>
-            </Helmet>
-            <h1 class="home-header">
-                Welcome to the web where your adventures can come true
-            </h1>
-            <Search />
-            <hr />
-            <div className="foto-principal"></div>
-        </div>
-    );
+  return (
+    <div className='home' id='home'>
+      <Helmet>
+        <title>Experiencias diferentes - Home</title>
+      </Helmet>
+      <h1 class='home-header'>
+        Welcome to the web where your adventures can come true
+      </h1>
+      <Search />
+      <hr />
+      <div className='foto-principal'></div>
+    </div>
+  );
 }
 export default Home;
